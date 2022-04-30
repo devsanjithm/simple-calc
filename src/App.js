4acd9037832e908686d4ad3fd7b11d1df015c9ed
SANJITH M <dev.sanjithm@gmail.com>
2022-04-30 17:43:19 +0530

    last

--- src/App.js
+++ src/App.js
@@ -1,58 +1,36 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Navigate, Route } from "react-router-dom";
-import Login from "./Component/login"
-import Signup from "./Component/signup";
-import Home from "./Component/Home";
-import { ContextProvider } from "./context";
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useState } from "react";
 
-<<<<<<< HEAD
-function App() {
-	return (
-		<ContextProvider>
-			<Router>
-				<Routes>
-					<Route exact path="/" element={<Navigate to="/login" />} />
-					<Route exact path="/login" element={<Login />} />
-					<Route exact path="/signup" element={<Signup />} />
-					<Route exact path="/Home" element={<Home />} />
-				</Routes>
-			</Router>
-		</ContextProvider>
-	);
-=======
-
-const App = ()=> {
+const App = () => {
   const [result, setResult] = useState("");
   const [res, setRes] = useState("calc-screen");
-  const [calscreen,setCalscreen] = useState("calc-app1");
-  const [key,setKey] = useState("keypad1");
-  const [inputar,setInputar] = useState("w1");
-  const [btn,setBtn] = useState("button");
-  const[oper,setOper] = useState("btn1");
-  const [zero,setZero] = useState("zeros1");
-  const [answer,setAnswer] = useState("");
-  const [ansscreen,setAnsscreen] = useState("calc-screen3");
-  
+  const [calscreen, setCalscreen] = useState("calc-app1");
+  const [key, setKey] = useState("keypad1");
+  const [inputar, setInputar] = useState("w1");
+  const [btn, setBtn] = useState("button");
+  const [oper, setOper] = useState("btn1");
+  const [zero, setZero] = useState("zeros1");
+  const [answer, setAnswer] = useState("");
+  const [ansscreen, setAnsscreen] = useState("calc-screen3");
+
   function handleclick(e) {
-      setResult(result.concat(e.target.name));
-      
-    if(result.length>=8){
+    setResult(result.concat(e.target.name));
+
+    if (result.length >= 8) {
       setRes("calc-screen1")
     }
-    if(result.length>=12){
+    if (result.length >= 12) {
       setRes("calc-screen2")
     }
   }
   function backspace() {
     setResult(result.slice(0, result.length - 1))
-    if(result.length>=12){
+    if (result.length >= 12) {
       setRes("calc-screen2")
     }
-    if(result.length>=8 && result.length<12){
+    if (result.length >= 8 && result.length < 12) {
       setRes("calc-screen1")
     }
-    if(result.length<8){
+    if (result.length < 8) {
       setRes("calc-screen")
     }
     setAnswer(answer.slice(0, answer.length - 1))
@@ -64,20 +42,20 @@ const App = ()=> {
   }
   function calculate() {
     try {
-     
+
       setResult("");
-       // eslint-disable-next-line
+      // eslint-disable-next-line
       setAnswer(eval(result).toString());
-      
+
     }
     catch (error) {
       setResult("");
       setAnswer("⚠er");
     }
   }
-  function darkmode(){
+  function darkmode() {
     var e = document.getElementById("checked").checked
-    if(e){
+    if (e) {
       setCalscreen("calc-app")
       setInputar("w")
       setKey("keypad")
@@ -85,8 +63,8 @@ const App = ()=> {
       setOper("btn2")
       setZero("zeros")
       setAnsscreen("calc-screen4")
-      document.getElementById("mode").innerHTML="Dark mode";
-    }else{
+      document.getElementById("mode").innerHTML = "Dark mode";
+    } else {
       setCalscreen("calc-app1")
       setInputar("w1")
       setKey("keypad1")
@@ -94,57 +72,56 @@ const App = ()=> {
       setOper("btn1")
       setZero("zeros1")
       setAnsscreen("calc-screen3")
-      document.getElementById("mode").innerHTML="Light mode";
+      document.getElementById("mode").innerHTML = "Light mode";
     }
   }
 
   return (
     <div>
-      
+
       <h1>SIMPLE CALCULATOR</h1>
-    <div className={calscreen}>
-      <form className="input">
-        <input type="text" id={inputar} className={res} disabled value={result}  />
-        <input type="text" className={ansscreen} disabled value={answer}  />
-      </form>
-      <div className={key}>
-        <button className={zero}  onClick={clear}>AC</button>
-        <button className={zero} onClick={backspace}>C</button>
-        <button className={oper} name="%" onClick={handleclick}>%</button>
-        <button className={oper}  name="/" onClick={handleclick}>/</button>
+      <div className={calscreen}>
+        <form className="input">
+          <input type="text" id={inputar} className={res} disabled value={result} />
+          <input type="text" className={ansscreen} disabled value={answer} />
+        </form>
+        <div className={key}>
+          <button className={zero} onClick={clear}>AC</button>
+          <button className={zero} onClick={backspace}>C</button>
+          <button className={oper} name="%" onClick={handleclick}>%</button>
+          <button className={oper} name="/" onClick={handleclick}>/</button>
 
-        <button className={btn} name="7" onClick={handleclick}>7</button>
-        <button className={btn} name="8" onClick={handleclick}>8</button>
-        <button className={btn} name="9" onClick={handleclick}>9</button>
-        <button className={oper}  name="*" onClick={handleclick}>*</button>
+          <button className={btn} name="7" onClick={handleclick}>7</button>
+          <button className={btn} name="8" onClick={handleclick}>8</button>
+          <button className={btn} name="9" onClick={handleclick}>9</button>
+          <button className={oper} name="*" onClick={handleclick}>*</button>
 
-        <button className={btn} name="4" onClick={handleclick}>4</button>
-        <button className={btn} name="5" onClick={handleclick}>5</button>
-        <button className={btn} name="6" onClick={handleclick}>6</button>
-        <button className={oper}  name="+" onClick={handleclick}>+</button>
+          <button className={btn} name="4" onClick={handleclick}>4</button>
+          <button className={btn} name="5" onClick={handleclick}>5</button>
+          <button className={btn} name="6" onClick={handleclick}>6</button>
+          <button className={oper} name="+" onClick={handleclick}>+</button>
 
-        <button className={btn} name="1" onClick={handleclick}>1</button>
-        <button className={btn} name="2" onClick={handleclick}>2</button>
-        <button className={btn} name="3" onClick={handleclick}>3</button>
-        <button className={oper}  name="-" onClick={handleclick}>-</button>
+          <button className={btn} name="1" onClick={handleclick}>1</button>
+          <button className={btn} name="2" onClick={handleclick}>2</button>
+          <button className={btn} name="3" onClick={handleclick}>3</button>
+          <button className={oper} name="-" onClick={handleclick}>-</button>
 
-        <button className={zero} name="00" onClick={handleclick}>00</button>
-        <button className={btn} name="0" onClick={handleclick}>0</button>
-        <button className={zero}  name="." onClick={handleclick}>.</button>
-        <button className="equal"   onClick={calculate}>=</button>
+          <button className={zero} name="00" onClick={handleclick}>00</button>
+          <button className={btn} name="0" onClick={handleclick}>0</button>
+          <button className={zero} name="." onClick={handleclick}>.</button>
+          <button className="equal" onClick={calculate}>=</button>
+        </div>
+      </div>
+      <div className="mg">
+        <p id="mode" >Light mode</p>
+        <label className="switch">
+          <input type="checkbox" id="checked" onClick={darkmode} ></input>
+          <span className="slider"></span>
+        </label>
+
       </div>
     </div>
-    <div className="mg">
-      <p id = "mode" >Light mode</p>
-       <label className="switch">
-      <input type="checkbox" id="checked" onClick={darkmode} ></input>
-      <span className="slider"></span>
-      </label>
-      
-      </div>
-  </div>
   );
->>>>>>> parent of da8f4e9 (solution improve)
 }
 
 export default App;
